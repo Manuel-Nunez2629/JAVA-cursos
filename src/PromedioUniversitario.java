4274aa5190e2a26a549cbcb971bb59d830fe87e8
Nanu <manuelnunez2629@gmail.com>
2024-06-28 12:25:01 -0500

    Promedio

--- src/PromedioUniversitario.java
+++ src/PromedioUniversitario.java
@@ -2,9 +2,16 @@ import java.util.Scanner;
 public class PromedioUniversitario {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hola este es mi promedio");
-        String Hola = sc.nextLine();
-        System.out.println(Hola);
+        float prom= 0, sumatoria = 0;
+        System.out.println("Ingrese su Nombre: ");
+        String nombre = sc.nextLine();
+        for(int i =1; i<=5; i++){
+            System.out.println("Ingrese sus notas:"+i);
+            float notas = sc.nextFloat();
+            sumatoria = sumatoria + notas;
+            prom = sumatoria/5;
+        }
+        System.out.println("Hola "+nombre+"\nTu promedio es de: "+prom);
 
     }
 }
