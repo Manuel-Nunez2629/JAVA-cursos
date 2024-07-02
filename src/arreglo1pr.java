8ef4619c0811ccf9afbe58833185a8d90e634894
Nanu <manuelnunez2629@gmail.com>
2024-07-01 21:06:36 -0500

    Curso Actualizado

--- src/arreglo1pr.java
+++ src/arreglo1pr.java
@@ -1,23 +1,29 @@
 /*Escribe un programa que lea un arreglo de enteros y encuentre el segundo mayor número en el arreglo.
 Ejemplo:
-Entrada: [12, 35, 1, 10, 34, 1]
+Entrada: [12, 35, 1, 10, 34, 3]
 Salida: El segundo mayor número es 34*/
+
+import java.util.Arrays;
 
 public class arreglo1pr {
     public static void main(String[] args) {
-        int [] array1 = {12, 35, 1, 10, 34, 1};
-        int segmenor=0;
-        int r=0;
+        int [] array1 = {12, 35, 1, 10, 34, 3};
+        int segmenor=0,mayor=0;
+        int posicion=0;
+        // Ordenar el arreglo en orden ascendente
+        Arrays.sort(array1);
+        mayor = array1[array1.length-2];
 
-        for(int i=0;i<array1.length;i++){
-            if(r<array1[1]){
-                r=array1[1];
-
-            }
+        System.out.println(mayor);
+        //Segundo mayor
 
 
-        }
+
+
+
 
 
     }
+
+
 }
